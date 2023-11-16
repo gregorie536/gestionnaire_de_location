8a1e14ced751488d1b14663ffed48f342720e082
gregorie536 <vexier.gregorie@gmail.com>
2023-11-16 15:03:21 +0100

    Update

---
+++ database/seeders/AppartementsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AppartementsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
